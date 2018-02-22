35fdcf6b6bb471f2e07d63f41f4ac385944bd0b8
Filip Adamczyk <Filip.Adamczyk@icrossing.com>
2018-02-22 10:10:20 -0800

    update app id

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var xhr = new XMLHttpRequest();
 
-document.getElementById("butthole").addEventListener("click", buttonClick, false);
+document.getElementById("app").addEventListener("click", buttonClick, false);
 
 function buttonClick() {
 	xhr.open('GET', 'https://traffic-owl.herokuapp.com/notification');
@@ -10,7 +10,7 @@ function buttonClick() {
 		var DONE = 4; // readyState 4 means the request is done.
 		var OK = 200; // status 200 is a successful return.
 		if (xhr.readyState === DONE) {
-			if (xhr.status === OK) 
+			if (xhr.status === OK)
 				console.log('Purple alert sent.'); // 'This is the returned text.'
 			} else {
 				console.log('Error: ' + xhr.status); // An error occurred during the request.
